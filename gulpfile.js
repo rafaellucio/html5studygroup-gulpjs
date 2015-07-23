f62369f7e13967107d96195e28e1e4896bde175e
Rafael Antonio Lucio <rafaelantoniolucio@gmail>
2015-07-23 17:58:52 -0300

    added index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function () {
+gulp.task('browser-sync', function () {
+	browserSync.init({
+		server: {
+			baseDir: './app'
+		}
+	});
+});
+
+gulp.task('default', ['browser-sync'], function () {
+
+	gulp.watch('./app/*.html').on('change', browserSync.reload);
 	console.log('It\'s working my default task');
 });
