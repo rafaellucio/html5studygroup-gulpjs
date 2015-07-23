67060dde3a741d98ea376dde9bce6a392d0d64a4
Rafael Antonio Lucio <rafaelantoniolucio@gmail>
2015-07-23 18:59:20 -0300

    added minify-cs task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var minifyCss = require('gulp-minify-css');
 var browserSync = require('browser-sync').create();
+
+gulp.task('minify-css', function () {
+	return gulp.src('./app/*.css')
+			.pipe(minifyCss({
+				compatibility: 'ie8'
+			}))
+			.pipe(gulp.dest('dist'));
+});
 
 gulp.task('browser-sync', function () {
 	browserSync.init({
@@ -9,8 +18,9 @@ gulp.task('browser-sync', function () {
 	});
 });
 
-gulp.task('default', ['browser-sync'], function () {
+gulp.task('default', ['minify-css','browser-sync'], function () {
 
 	gulp.watch('./app/*.html').on('change', browserSync.reload);
+	gulp.watch('./app/*.css').on('change', browserSync.reload);
 	console.log('It\'s working my default task');
 });
